65fe06ed0846c43c6496a73e28a0f44097626533
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-08-17 17:49:29 -0700

    script to dump github usernames

---
+++ db/github.js
@@ -0,0 +1,17 @@
+// mongo nodeknockout json2.js github.js | tail +5 | python -mjson.tool
+
+var teams = db.Team.find().map(function(t) {
+  return {
+    name: t.name,
+    members: t.members.map(function(m) {
+      var p = db.Person.findOne({ _id: m._id }, {
+        github: true, email: true, name: true
+      });
+      delete p._id;
+
+      return p;
+    })
+  };
+});
+
+print(JSON.stringify(teams));
