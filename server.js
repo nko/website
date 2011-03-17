8fab8e21ba0a50932d133b06ac8f2c3ea4a2cda1
Gerad Suyderhoud <gerad@fortnightlabs.com>
2011-03-17 15:15:19 -0700

    development and production settings; use local jquery in development

--- server.js
+++ server.js
@@ -4,23 +4,15 @@ var express = require('express')
   , pub = __dirname + '/public'
   , port = process.env.PORT || 8000;
 
+// express
 var app = express.createServer();
-
-app.use(require('stylus').middleware(pub));
-app.use(express.static(pub));
-app.use(express.logger());
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-
-app.listen(port);
-
 app.get('/', function(req, res) {
   res.render('index');
 });
+app.listen(port);
 
-var ws = io.listen(app)
+// socket.io
+var ws = io.listen(app);
 ws.on('connection', function(client) {
   client
     .on('message', function(data) {
@@ -37,3 +29,23 @@ ws.on('connection', function(client) {
 });
 
 util.log("listening on 0.0.0.0:" + port + ".");
+
+// config
+app.configure(function() {
+  app.use(require('stylus').middleware(pub));
+  app.use(express.logger());
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+});
+
+app.configure('development', function() {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.static(pub));
+  app.set('view options', { scope: { development: true }});
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+  app.use(express.static(pub, { maxAge: 1000 * 5 * 60 }));
+  app.set('view options', { scope: { development: false }});
+});
