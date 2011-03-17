a6a4cf8e40e0ccedb4b9967561efa4e3a550f7d1
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-03-16 22:54:48 -0700

    remind us that node is fast

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
-  , pub = __dirname + '/public';
+  , pub = __dirname + '/public'
+  , port = process.env.PORT || 8000;
 
 var app = express.createServer();
 
@@ -11,8 +12,10 @@ app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.listen(process.env.PORT || 8000);
+app.listen(port);
 
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+console.log("listening on 0.0.0.0:" + port + ".");
